51aad7997fa5c54010565e4a1fd19e02c35be22e
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-13 12:47:23 +0200

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Ngoni Katsidzira  on 13/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
